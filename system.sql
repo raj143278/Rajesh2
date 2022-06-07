cc2e13718b95f21c086a1e832cfaf498ff3b45b1
Rajesh <reddyrajesh537@gmail.com>
2022-06-07 12:33:19 +0530

    newdv

---
+++ system.sql
@@ -0,0 +1,15 @@
+select * from student7;
+
+
+select 123/345 from dual
+
+select 123 as numbers from dual;
+
+select 123 + 345  as addition from dual;
+
+select id idnumber from student7;
+
+select * from  student4;
+
+select * from student7
+where id =115;
